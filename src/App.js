27f8a771f5c5ee9edd1daf0bdac8945529c6cd6c
Najat76 <safari76@hotmail.co.uk>
2023-06-07 00:49:06 +0300

    not showing specific trip detail

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import './App.css';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import TripsList from './components/TripsList';
-import TripDetail from './components/TripDetail';
+import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import TripsList from "./components/TripsList";
+import TripDetail from "./components/TripDetail";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div id="page-top">
       <Nav />
-      <Home />
-      <TripsList />
-      <TripDetail />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trips" element={<TripsList />} />
+        <Route path="/trip-detail/:tripId" element={<TripDetail />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+// Bonus: https://stackoverflow.com/questions/42659611/reactjs-use-slug-instead-of-id-in-the-url-with-react-router
